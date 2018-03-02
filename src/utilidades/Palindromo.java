4294b504e797271b27289c756bc12c906d5ae455
Alexandre Insua <alexandreinsua@gmail.com>
2018-03-02 20:33:26 +0100

    Clase palíndromo

---
+++ src/utilidades/Palindromo.java
@@ -0,0 +1,30 @@
+package utilidades;
+
+public class Palindromo {
+
+	public boolean espalindromo(String cadena) {
+		boolean valor = true;
+		int i,j, k;
+		String cadena2 = "";
+		// quitamos los espacios
+		for (i = 0; i < cadena.length(); i++) {
+			if (cadena.charAt(i) != ' ')
+				cadena2 += cadena.charAt(i);
+		}
+		// volvemos a asignar variables
+		cadena = cadena2;
+		k = cadena.length();
+		// comparamos cadenas
+		for (j = 0; j < (cadena.length()); j++) {
+			if (cadena.substring(j, j + 1).equals(cadena.substring(k - 1, k)) == false) {
+				// si una sola letra no corresponde no es un palindromo por
+				// tanto
+				// sale del ciclo con valor false
+				valor = false;
+				break;
+			}
+			k--;
+		}
+		return valor;
+	}
+}
