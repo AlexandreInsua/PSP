a8c813a35b4dab92f46aac77995540953393506f
Alexandre Insua <alexandreinsua@gmail.com>
2018-03-02 20:52:24 +0100

    Repaso clases url

--- src/b02comunicacions01/Principal.java
+++ src/b02comunicacions01/Principal.java
@@ -12,9 +12,9 @@ import java.util.Map;
 
 public class Principal {
 	public static void main(String[] args) throws IOException {
-		// testInetAddress();
+		 // testInetAddress();
 
-		// visualizarPropiedadesURL();
+		 visualizarPropiedadesURL();
 
 		// propiedadesURL2();
 
@@ -22,24 +22,192 @@ public class Principal {
 		// exemploURLConnection();
 
 		// exemploURLConnection2();
-		 exemploURLConnection3();
-
+		// exemploURLConnection3();
 
 	}
 
-
-
-	private static void exemplo04Sockets() {
-		
-		
+	// CLASE INETADDRESS
+	private static void testInetAddress() {
+		InetAddress dir = null;
+		System.out.println("======================");
+		System.out.println("SAIDA PARA LOCALHOST: ");
+	
+		try {
+			// Localhost
+			System.out.println("======================");
+			System.out.println("SAIDA PARA LOCALHOST: ");
+			dir = InetAddress.getByName("User-PC"); // obten a dirección a partir do nome de equipo
+			probaMetodos(dir);
+	
+			System.out.println("======================");
+			System.out.println("SAIDA PARA UNHA URL: ");
+			dir = InetAddress.getByName("www.google.es");
+			probaMetodos(dir);
+	
+			System.out.println("DIRECCIÓNS IP PARA: " + dir.getHostName());
+			InetAddress[] direccions = InetAddress.getAllByName(dir.getHostName());
+	
+			for (int i = 0; i < direccions.length; i++) {
+				System.out.println(direccions[i].toString());
+	
+			}
+		} catch (UnknownHostException uhe) {
+			uhe.printStackTrace();
+		}
+	
 	}
 
+	private static void probaMetodos(InetAddress dir) {
+		System.out.println("Método getByName(): " + dir);
+		InetAddress dir2;
+	
+		try {
+			dir2 = InetAddress.getLocalHost();
+	
+		} catch (UnknownHostException uhe) {
+			System.out.println(uhe);
+		}
+	
+		// Usamos o método das clases
+		System.out.println("Método getHostname(): " + dir.getHostName());
+		System.out.println("Método getHostAddress(): " + dir.getHostAddress());
+		System.out.println("Método toString(): " + dir.toString());
+		System.out.println("Método getCannonicalHostName(): " + dir.getCanonicalHostName());
+	}
 
+	private static void visualizar(URL url) {
+	
+		System.out.println("Url completa: " + url.toString());
+		System.out.println("Protocolo: " + url.getProtocol());
+		System.out.println("host: " + url.getHost());
+		System.out.println("Porto: " + url.getPort());
+		System.out.println("Ficheiro: " + url.getFile());
+		System.out.println("Info Usuario: " + url.getUserInfo());
+		System.out.println("Path: " + url.getPath());
+		System.out.println("Dominio: " + url.getAuthority());
+		System.out.println("Consulta: " + url.getQuery());
+		System.out.println("==================");
+	}
+
+	private static void propiedadesURL2() {
+		// Conecta co site http://www.elatozano.es
+		URL url = null;
+		BufferedReader in;
+		try {
+			url = new URL("http://www.elaltozano.es");
+	
+			InputStream is = url.openStream();
+			in = new BufferedReader(new InputStreamReader(is));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			in.close();
+		} catch (MalformedURLException murle) {
+			murle.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	
+	}
+
+	// CLASE URL
+	private static void visualizarPropiedadesURL() {
+		URL url;
+	
+		try {
+			System.err.println("Contrutor simple para unha URL: ");
+			url = new URL("http://docs.oracle.com/");
+			visualizar(url);
+	
+			System.err.println("Outro contrutor");
+			url = new URL("http", "docs.oracle.com", "/javase/7");
+			visualizar(url);
+	
+			System.err.println("Contrutor para protocolo + url + porto + directorio");
+			url = new URL("http", "docs.oracle.com", 80, "/javase/7");
+			visualizar(url);
+	
+			System.err.println("Contrutor para un obxecto url e un directorio");
+			URL urlbase = new URL("http://docs.oracle.com/");
+			url = new URL(urlbase, "/javase/7/docs/api/java/net/URL.html");
+			visualizar(url);
+	
+			url = new URL("https://docs.oracle.com/javase/9/whatsnew/JSNEW.pdf");
+			visualizar(url);
+	
+		} catch (MalformedURLException e) {
+	
+			e.printStackTrace();
+		}
+	
+	}
+
+	private static void exemploURLConnection() {
+		URL url = null;
+		URLConnection urlCon = null;
+		BufferedReader br = null;
+		InputStream is = null;
+		String inputLine = null;
+	
+		try {
+			// crea un recurso
+			url = new URL("http://www.elaltozano.es");
+			// abre conexión
+			urlCon = url.openConnection();
+	
+			is = urlCon.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is));
+	
+			while ((inputLine = br.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			br.close();
+		} catch (MalformedURLException murle) {
+			murle.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	private static void exemploURLConnection2() {
+	
+		try {
+			// crea un recurso
+			URL url = new URL("http://localhost/php/verNome.php");
+			// abre conexión
+			URLConnection urlCon = url.openConnection();
+			urlCon.setDoOutput(true);
+			String cadea = "nome=Alexandre&apelidos=Insua Moreira";
+	
+			// ESCRIBIR NA URL
+			PrintWriter output = new PrintWriter(urlCon.getOutputStream());
+	
+			output.write(cadea);
+			output.close();
+	
+			BufferedReader br = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				System.out.println(linha);
+			}
+			br.close();
+		} catch (MalformedURLException murle) {
+			murle.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	
+	}
 
 	private static void exemploURLConnection3() throws IOException {
+		
 		String cadena;
+		
 		URL url = new URL("http://localhost/php/verNome.html");
+		
 		URLConnection conexion = url.openConnection();
+		
 		System.out.println("Direccion [getURD()]:" + conexion.getURL());
 		Date fecha = new Date(conexion.getLastModified());
 		System.out.println("Fecha ultima modificacion [getLastModified()]: " + fecha);
@@ -63,180 +231,8 @@ public class Principal {
 				System.out.println(cadena);
 			} //
 		}
-
+	
 	}
 
-	private static void exemploURLConnection2() {
-
-		try {
-			// crea un recurso
-			URL url = new URL("http://localhost/php/verNome.php");
-			// abre conexión
-			URLConnection urlCon = url.openConnection();
-			urlCon.setDoOutput(true);
-			String cadea = "nome=Alexandre&apelidos=Insua Moreira";
-
-			// ESCRIBIR NA URL
-			PrintWriter output = new PrintWriter(urlCon.getOutputStream());
-
-			output.write(cadea);
-			output.close();
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
-			String linha;
-			while ((linha = br.readLine()) != null) {
-				System.out.println(linha);
-			}
-			br.close();
-		} catch (MalformedURLException murle) {
-			murle.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-
-	}
-
-	private static void exemploURLConnection() {
-		URL url = null;
-		URLConnection urlCon = null;
-		BufferedReader br = null;
-		InputStream is = null;
-		String inputLine = null;
-
-		try {
-			// crea un recurso
-			url = new URL("http://www.elaltozano.es");
-			// abre conexión
-			urlCon = url.openConnection();
-
-			is = urlCon.getInputStream();
-			br = new BufferedReader(new InputStreamReader(is));
-
-			while ((inputLine = br.readLine()) != null) {
-				System.out.println(inputLine);
-			}
-			br.close();
-		} catch (MalformedURLException murle) {
-			murle.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-
-	private static void propiedadesURL2() {
-		// Conecta co site http://www.elatozano.es
-		URL url = null;
-		BufferedReader in;
-		try {
-			url = new URL("http://www.elaltozano.es");
-
-			InputStream is = url.openStream();
-			in = new BufferedReader(new InputStreamReader(is));
-			String inputLine;
-			while ((inputLine = in.readLine()) != null) {
-				System.out.println(inputLine);
-			}
-			in.close();
-		} catch (MalformedURLException murle) {
-			murle.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-
-	}
-
-	// CLASE URL
-	private static void visualizarPropiedadesURL() {
-		URL url;
-
-		try {
-			System.err.println("Contrutor simple para unha URL: ");
-			url = new URL("http://docs.oracle.com/");
-			visualizar(url);
-
-			System.err.println("Outro contrutor");
-			url = new URL("http", "docs.oracle.com", "/javase/7");
-			visualizar(url);
-
-			System.err.println("Contrutor para protocolo + url + porto + directorio");
-			url = new URL("http", "docs.oracle.com", 80, "/javase/7");
-			visualizar(url);
-
-			System.err.println("Contrutor para un obxecto url e un directorio");
-			URL urlbase = new URL("http://docs.oracle.com/");
-			url = new URL(urlbase, "/javase/7/docs/api/java/net/URL.html");
-			visualizar(url);
-
-			url = new URL("https://docs.oracle.com/javase/9/whatsnew/JSNEW.pdf");
-			visualizar(url);
-
-		} catch (MalformedURLException e) {
-
-			e.printStackTrace();
-		}
-
-	}
-
-	private static void visualizar(URL url) {
-
-		System.out.println("Url completa: " + url.toString());
-		System.out.println("Protocolo: " + url.getProtocol());
-		System.out.println("host: " + url.getHost());
-		System.out.println("Porto: " + url.getPort());
-		System.out.println("Ficheiro: " + url.getFile());
-		System.out.println("Info Usuario: " + url.getUserInfo());
-		System.out.println("Path: " + url.getPath());
-		System.out.println("Dominio: " + url.getAuthority());
-		System.out.println("Consulta: " + url.getQuery());
-		System.out.println("==================");
-	}
-
-	// CLASE INETADDRESS
-	private static void testInetAddress() {
-		InetAddress dir = null;
-		System.out.println("======================");
-		System.out.println("SAIDA PARA LOCALHOST: ");
-
-		try {
-			// Localhost
-			System.out.println("======================");
-			System.out.println("SAIDA PARA LOCALHOST: ");
-			dir = InetAddress.getByName("User-PC");
-			probaMetodos(dir);
-
-			System.out.println("======================");
-			System.out.println("SAIDA PARA UNHA URL: ");
-			dir = InetAddress.getByName("www.google.es");
-			probaMetodos(dir);
-
-			System.out.println("DIRECCIÓNS IP PARA: " + dir.getHostName());
-			InetAddress[] direccions = InetAddress.getAllByName(dir.getHostName());
-
-			for (int i = 0; i < direccions.length; i++) {
-				System.out.println(direccions[i].toString());
-
-			}
-		} catch (UnknownHostException uhe) {
-			uhe.printStackTrace();
-		}
-
-	}
-
-	private static void probaMetodos(InetAddress dir) {
-		System.out.println("Método getByName(): " + dir);
-		InetAddress dir2;
-
-		try {
-			dir2 = InetAddress.getLocalHost();
-
-		} catch (UnknownHostException uhe) {
-			System.out.println(uhe);
-		}
-
-		// Usamos o método das clases
-		System.out.println("Método getHostname(): " + dir.getHostName());
-		System.out.println("Método getHostAddress(): " + dir.getHostAddress());
-		System.out.println("Método toString(): " + dir.toString());
-		System.out.println("Método getCannonicalHostName(): " + dir.getCanonicalHostName());
-	}
+	
 }
